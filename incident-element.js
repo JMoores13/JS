a754746889b715046d8a018a8e6a676e0dddc3e4
Jacob Moores <128637985+JMoores13@users.noreply.github.com>
2025-10-27 13:13:50 -0400

    Update edit URL format in renderIncident method

--- incident-element.js
+++ incident-element.js
@@ -170,7 +170,8 @@ connectedCallback() {
     const isExpanded = this.expandedIds.has(String(i.id));
     const capitalize = (str) =>
       typeof str === "string" ? str.charAt(0).toUpperCase() + str.slice(1) : str;
-    const editUrl = `/e/Incident-Reporting-Tool/incident-view/34236/${i.id}`;
+    const layoutId = "6a914ae4-95d9-d573-1b7a-21bb188f7457";
+    const editUrl = `/w/incident-reporting-tool/${i.id}/${layoutId}`;
 
     if (!isExpanded) {
       return `
