54d29ce115b221b8d3b0950d81f4be11d727588c
Jacob Moores <128637985+JMoores13@users.noreply.github.com>
2025-11-04 15:13:55 -0500

    Update map-place.js

--- map-place.js
+++ map-place.js
@@ -18,7 +18,7 @@ class MarkerMapElement extends HTMLElement {
           padding: 4px 8px; 
           color: white;
           background-color: #0b5fff;
-          border-style: solid;
+          border-style: solid #0b5fff;
           border-width: 0.0625rem;
           border-radius: 0.25rem;
           display: inline-block;
